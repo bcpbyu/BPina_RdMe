a2290588363b738fded999ded16ce9e2650e9987
Pina <brannoncpina@gmail.com>
2021-03-13 13:33:59 -1000

    ReadMe questions added

--- index.js
+++ index.js
@@ -1,5 +1,56 @@
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title or your project? ',
+    },
+    {
+        type: 'input',
+        name: 'desc',
+        message: 'A brief description of the project: ',
+    },
+    {
+        type: 'input',
+        name: 'intall',
+        message: 'Instrustions to install: ',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage info: ',
+    },
+    {
+        type: 'input',
+        name: 'contr',
+        message: 'Guidelines for contribution: ',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Test instructions: ',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What is your project license? ',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'NONE'],
+    },
+    {
+        type: 'input',
+        name: 'git',
+        message: 'What is your github username? ',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email? ',
+    },
+    {
+        type: 'input',
+        name: '',
+        message: '',
+    },
 
 ];
 
